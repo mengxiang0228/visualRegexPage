bedbec48ebc0030114ff98de2c325e5d0b99c91d
wangwl <wwl_github@163.com>
2021-04-10 14:03:36 +0800

    fix: logInput中，textarea和holder的字体不一致

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -5,9 +5,9 @@ module.exports = {
         contentBase: resolve(__dirname, './dist'),
         port: 9000,
         proxy: {
-            '/static/comment': 'http://localhost:8000',
-            '/static/comment/index.css': 'http://localhost:8000',
-            '/comment': 'http://localhost:8000',
+            '/static/comment': 'http://localhost:80',
+            '/static/comment/index.css': 'http://localhost:80',
+            '/comment': 'http://localhost:80',
         },
     },
 }
